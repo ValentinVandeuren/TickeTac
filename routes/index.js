f44bd15853ccccd38f5b6151ad546f4047099948
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-04-14 16:38:50 +0200

    recup des infos du ticket du front end au back end

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 let journeyModel = require('../models/journey');
 
-
-
 /* GET login page. */
 router.get('/', function (req, res, next) {
   res.render('index');
@@ -18,8 +16,26 @@ router.post('/notrain', function (req, res, next) {
   res.render('noTrain');
 });
 
-router.post('/journey-list', function (req, res, next) {
-  res.render('journeyList');
+router.post('/journey-list', async function (req, res, next) {
+
+  var destinationForm = req.body.destination;
+  var arrivalForm = req.body.arrival;
+  var dateForm = req.body.date;
+  var journeyList = await journeyModel.find({
+    departure: destinationForm
+  });
+  var newDate = new Date(dateForm);
+
+
+  for (var i = 0; i < journeyList.length; i++) {
+    if (journeyList[i].arrival == arrivalForm && journeyList[i].date.getDate() == newDate.getDate()) {
+      res.render('journeyList');
+      break;
+    } else {
+      res.redirect('/notrain')
+      break;
+    }
+  }
 });
 
 router.get('/basket', function (req, res, next) {
