644003d8fe69fe45456a8f08174730ac6232bb78
Valentin Vandeuren <valentinvandeuren.code@gmail.com>
2022-04-14 09:37:01 +0200

    corrrection base

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var models = require('./routes/index')
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,98 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+
+// useNewUrlParser ;)
+var options = {
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ };
+
+// --------------------- BDD -----------------------------------------------------
+mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+   options,
+   function(err) {
+    if (err) {
+      console.log(`error, failed to connect to the database because --> ${err}`);
+    } else {
+      console.info('*** Database Ticketac connection : Success ***');
+    }
+   }
+);
+
+var journeySchema = mongoose.Schema({
+  departure: String,
+  arrival: String,
+  date: Date,
+  departureTime: String,
+  price: Number,
+});
+
+var journeyModel = mongoose.model('journey', journeySchema);
+
+var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
+var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+// Remplissage de la base de donnée, une fois suffit
+router.get('/save', async function(req, res, next) {
+
+  // How many journeys we want
+  var count = 300
+
+  // Save  ---------------------------------------------------
+    for(var i = 0; i< count; i++){
+
+    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+
+    if(departureCity != arrivalCity){
+
+      var newUser = new journeyModel ({
+        departure: departureCity , 
+        arrival: arrivalCity, 
+        date: date[Math.floor(Math.random() * Math.floor(date.length))],
+        departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
+        price: Math.floor(Math.random() * Math.floor(125)) + 25,
+      });
+       
+       await newUser.save();
+
+    }
+
+  }
+  res.render('index', { title: 'Express' });
+});
+
+
+// Cette route est juste une verification du Save.
+// Vous pouvez choisir de la garder ou la supprimer.
+router.get('/result', function(req, res, next) {
+
+  // Permet de savoir combien de trajets il y a par ville en base
+  for(i=0; i<city.length; i++){
+
+    journeyModel.find( 
+      { departure: city[i] } , //filtre
+  
+      function (err, journey) {
+
+          console.log(`Nombre de trajets au départ de ${journey[0].departure} : `, journey.length);
+      }
+    )
+
+  }
+
+
   res.render('index', { title: 'Express' });
 });
 
