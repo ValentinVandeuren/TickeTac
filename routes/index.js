9dbeffd382c061bda965828234952083003792e4
Valentin Vandeuren <valentinvandeuren.code@gmail.com>
2022-04-15 11:00:46 +0200

    basket dynamique

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,9 @@ router.post('/journey-list', async function (req, res, next) {
   var newDate = new Date(dateForm);
   let dayOfDate = newDate.getDate();
   let MonthOfDate = newDate.getMonth();
-  let dateMonthDay = `${dayOfDate} / ${MonthOfDate +1}`;
+  let yearOfDate = newDate.getFullYear();
+  let dateMonthDay = `${dayOfDate}/${MonthOfDate +1}`;
+  let fullDate = `${dayOfDate}/${MonthOfDate +1}/${yearOfDate}`;
   let listOfJourney = []
 
 
@@ -37,14 +39,33 @@ router.post('/journey-list', async function (req, res, next) {
     }
   }
 
-  res.render('journeyList', { listOfJourney, dateMonthDay })
+  res.render('journeyList', { listOfJourney, dateMonthDay, fullDate })
 });
 
 router.get('/basket', function (req, res, next) {
   if(req.session.user == null){
     res.redirect('/')
   } else {
-    res.render('basket');
+    if(!req.session.basketList) req.session.basketList = [];
+    var status = false;
+
+    for (var i = 0; i < req.session.basketList.length; i++) {
+      if (req.session.basketList[i].departureTime == req.query.time && req.session.basketList[i].date == req.query.date) {
+        status = true;
+        console.log('coucou');
+      }
+    }
+
+    if (status == false) {
+      req.session.basketList.push({
+        departure: req.query.departure,
+        arrival: req.query.arrival,
+        date: req.query.date,
+        departureTime: req.query.time,
+        price: req.query.price
+      });
+    }
+    res.render('basket', { basketList: req.session.basketList });
   }
 });
 
